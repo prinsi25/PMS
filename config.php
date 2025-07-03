c382de01c4a46a5d7d8d1257205ec6c3df91146e
asha <ashakheni@gmail.com>
2025-07-03 10:44:15 +0530

    first

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$host = "localhost";        
+$user = "root";             
+$password = "";             
+$database = "pharmacy";     
+
+$con = mysqli_connect($host, $user, $password, $database);
+
+
+?>
+
+
---
+++ navigation.php
@@ -0,0 +1,75 @@
+<nav class="pcoded-navbar">
+        <div class="navbar-wrapper">
+            <div class="navbar-brand header-logo">
+                <a href="index-2.php" class="b-brand">
+                    <div class="b-bg">
+                        <i class="feather icon-trending-up"></i>
+                    </div>
+                    <span class="b-title">Pharmacy</span>
+                </a>
+                <a class="mobile-menu" id="mobile-collapse" href="javascript:"><span></span></a>
+            </div>
+            <div class="navbar-content scroll-div">
+                <ul class="nav pcoded-inner-navbar">
+                    <!-- <li class="nav-item pcoded-menu-caption">
+                        <label>Navigation</label>
+                    </li> -->
+                    
+                    <li data-username="Authentication Sign up Sign in reset password Change password Personal information profile settings map form subscribe" class="nav-item pcoded-hasmenu">
+                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-lock"></i></span><span class="pcoded-mtext">User Authentication</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="auth-signin.php" class="" target="_blank">Admin Login</a></li>
+                            <li class=""><a href="auth-signup.php" class="" target="_blank">Admin Logout</a></li>
+                            <li class=""><a href="auth-signup.php" class="" target="_blank">Cashier Login</a></li>
+                        </ul>
+                    </li>
+                    <li data-username="dashboard Default Ecommerce CRM Analytics Crypto Project" class="nav-item pcoded-hasmenu">
+                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="#" class="">Total Medicines Stock</a></li>
+                            <li class=""><a href="#" class="">Daily/Mounthly Sales Seport</a></li>
+                            <li class=""><a href="#" class="">Expiry Alert</a></li>
+                        </ul>
+                    </li>
+                   
+                    <li data-username="basic components Button Alert Badges breadcrumb Paggination progress Tooltip popovers Carousel Cards Collapse Tabs pills Modal Grid System Typography Extra Shadows Embeds" class="nav-item pcoded-hasmenu">
+                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-box"></i></span><span class="pcoded-mtext">Medicine Inventory</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="#" class="">Add/Edit/delete/update Medicine</a></li>
+                            <li class=""><a href="#" class="">Price</a></li>
+                            <li class=""><a href="#" class="">Expiry Date</a></li>
+                            <li class=""><a href="#" class="">Batch Number & Brand</a></li>
+                            <li class=""><a href="#" class="">GST/Schedule Details</a></li>
+                            <li class=""><a href="#" class="">Low-Stock Notification</a></li>
+                        </ul>
+                    </li>
+                    <!-- <li class="nav-item pcoded-menu-caption">
+                        <label>Forms & table</label>
+                    </li>
+                    <li data-username="form elements advance componant validation masking wizard picker select" class="nav-item">
+                        <a href="form_elements.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Form elements</span></a>
+                    </li>
+                    <li data-username="Table bootstrap datatable footable" class="nav-item">
+                        <a href="tbl_bootstrap.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-server"></i></span><span class="pcoded-mtext">Table</span></a>
+                    </li>
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Chart & Maps</label>
+                    </li>
+                    <li data-username="Charts Morris" class="nav-item"><a href="chart-morris.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-pie-chart"></i></span><span class="pcoded-mtext">Chart</span></a></li>
+                    <li data-username="Maps Google" class="nav-item"><a href="map-google.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-map"></i></span><span class="pcoded-mtext">Maps</span></a></li>
+                    <li class="nav-item pcoded-menu-caption">
+                        <label>Pages</label>
+                    </li>
+                    <li data-username="Authentication Sign up Sign in reset password Change password Personal information profile settings map form subscribe" class="nav-item pcoded-hasmenu">
+                        <a href="javascript:" class="nav-link "><span class="pcoded-micon"><i class="feather icon-lock"></i></span><span class="pcoded-mtext">Authentication</span></a>
+                        <ul class="pcoded-submenu">
+                            <li class=""><a href="auth-signup.php" class="" target="_blank">Sign up</a></li>
+                            <li class=""><a href="auth-signin.php" class="" target="_blank">Sign in</a></li>
+                        </ul>
+                    </li> -->
+                    <!-- <li data-username="Sample Page" class="nav-item"><a href="sample-page.php" class="nav-link"><span class="pcoded-micon"><i class="feather icon-sidebar"></i></span><span class="pcoded-mtext">Sample page</span></a></li>
+                    <li data-username="Disabled Menu" class="nav-item disabled"><a href="javascript:" class="nav-link"><span class="pcoded-micon"><i class="feather icon-power"></i></span><span class="pcoded-mtext">Disabled menu</span></a></li>
+                </ul>  -->
+            </div>
+        </div>
+    </nav>
